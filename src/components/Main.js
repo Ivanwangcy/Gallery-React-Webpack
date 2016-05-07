8509ffaeef2d2d6bdb737eaf88bbba051b6708bf
Ivan <wcy5211678@163.com>
2016-05-07 22:41:02 +0800

    update project

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,19 @@
 require('normalize.css/normalize.css'); // 比较流行的 css reset
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
 
 let yeomanImage = require('../images/yeoman.png');
+// 获取图片相关的数据
+let imagesData = require('../data/imagesData.json'); // imagesData.json 文件
 
+// 利用ES5的map函数, 添加imageURL图片路径信息
+imagesData = imagesData.map(function(value){
+  value.imageURL = require('../images/' + value.fileName);
+  return value;
+});
+
+// Gallery by React App
 class AppComponent extends React.Component {
   render() {
     return (
@@ -12,6 +21,10 @@ class AppComponent extends React.Component {
         <img src={yeomanImage} alt="Yeoman Generator" />
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
       </div>
+      // <section className="stage">
+      //   <section className="img-sec"></section>
+      //   <nav className="controller-nav"></nav>
+      // </section>
     );
   }
 }
